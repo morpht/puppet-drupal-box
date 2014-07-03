3b518a1d71a66aae1c2a87baa92d3b6bdc9443f2
Marji Cermak <marji@morpht.com>
2014-07-03 16:15:38 +1000

    apt_src: parametrising codename. Removing dotdeb if not precise.

--- modules/drupal_sandbox/manifests/apt_src.pp
+++ modules/drupal_sandbox/manifests/apt_src.pp
@@ -21,27 +21,32 @@ class drupal_sandbox::apt_src {
   #
   class { 'apt': purge_sources_list => true }
 
+  if $::lsbdistcodename == '' {
+    fail('drupal_sandbox::apt_src: expecting ::lsbdistcodename')
+  }
+  $codename = $::lsbdistcodename
+
   # We want to get the region, e.g. us-west-1 or ap-southeast-2
-  # but at least in AU, it returns ap-southeast-2b 
+  # but at least in AU, it returns ap-southeast-2b
   # so we need to cut of the character behind -digit, if any:
   if $::ec2_placement_availability_zone =~ /^(.+-\d)[a-z]{0,1}$/ {
     $archive = "http://${1}.ec2.archive.ubuntu.com/ubuntu/"
   } else {
     $archive = "http://us.archive.ubuntu.com/ubuntu/"
   }
-  apt::source { 'precise':
+  apt::source { $codename:
     location   => $archive,
-    release    => 'precise',
+    release    => $codename,
     repos      => 'main universe multiverse',
   }
-  apt::source { 'precise-updates':
+  apt::source { "${codename}-updates":
     location   => $archive,
-    release    => 'precise-updates',
+    release    => "${codename}-updates",
     repos      => 'main universe multiverse',
   }
-  apt::source { 'precise-security':
+  apt::source { "${codename}-security":
     location   => 'http://security.ubuntu.com/ubuntu',
-    release    => 'precise',
+    release    => "${codename}-security",
     repos      => 'main universe multiverse',
   }
   #
@@ -54,11 +59,14 @@ class drupal_sandbox::apt_src {
     key_server => 'keys.gnupg.net',
   }
 
-  apt::source { 'dotdeb':
-    location   => 'http://packages.dotdeb.org',
-    release    => 'squeeze',
-    repos      => 'all',
-    key        => '89DF5277',
-    key_source => 'http://www.dotdeb.org/dotdeb.gpg',
+  # only for precise, we use dotdeb repo:
+  if $codename == 'precise' {
+    apt::source { 'dotdeb':
+      location   => 'http://packages.dotdeb.org',
+      release    => 'squeeze',
+      repos      => 'all',
+      key        => '89DF5277',
+      key_source => 'http://www.dotdeb.org/dotdeb.gpg',
+    }
   }
 }
