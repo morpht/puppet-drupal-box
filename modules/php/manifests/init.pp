3bf7f473d35f9ac45e87463eb1fadd6a0fb5955a
Marji Cermak <marji@morpht.com>
2016-09-02 18:58:11 +1000

    php-fpm: log to /var/log/php5-errors.log e.g.: http://stackoverflow.com/questions/8677493/php-fpm-doesnt-write-to-error-log

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -38,6 +38,30 @@
 # [*fpm_max_spare_servers*]
 # pm.max_spare_servers for pool.d/www.conf. Only if php_engine is php5-fpm.
 #
+# [*fpm_logrotate_when*]
+# If it is not undef, /etc/logrotate.d/php will be created and the value
+# will be used as frequency / condition.
+# Example: weekly, daily, size 100k
+#
+# [*fpm_logrotate_rotate*]
+# Used only if fpm_logrotate_when above is defined.
+# How many log files to keep.
+#
+# [*php_error_log_path*]
+# Full path of a logfile for php error logs - output from php5-fpm workers.
+# It must not be the same file as /var/log/php5-fpm.log which belongs to
+# the php5-fpm service / daemon itself (which runs as root while workers
+# run as www-data, they also have different timestamp format).
+#
+# [*php_error_log_mode*]
+# File mode of the php error log file.
+#
+# [*php_error_log_owner*]
+# File owner of the php error log file.
+#
+# [*php_error_log_group*]
+# Group owner of the php error log file.
+#
 # [*ensure_php_debug_pkgs*]
 # whether to install php5-xdebug'
 # Valid values are: present, installed, absent, purged
@@ -62,6 +86,12 @@ class php (
   $fpm_start_servers     = 4,
   $fpm_min_spare_servers = 2,
   $fpm_max_spare_servers = 6,
+  $fpm_logrotate_rotate  = 12,
+  $fpm_logrotate_when    = 'weekly',
+  $php_error_log_path    = '/var/log/php5-errors.log',
+  $php_error_log_mode    = '0640',
+  $php_error_log_owner   = 'www-data',
+  $php_error_log_group   = 'www-data',
   $ensure_php_debug_pkgs = 'purged'
 ) {
 
@@ -158,6 +188,34 @@ class php (
     group   => 'root',
     mode    => '0444',
     require => Package['php5-suhosin'],
+  }
+  if $fpm_logrotate_when {
+    $reopenlogs = '/usr/local/bin/php5-fpm-reopenlogs'
+    file { 'php5-fpm-reopenlogs':
+      ensure  => present,
+      path    => $reopenlogs,
+      source  => 'puppet:///modules/php/php5-fpm-reopenlogs',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0555',
+    }
+    file { '/etc/logrotate.d/php5-fpm':
+      ensure  => present,
+      content => template('php/logrotate.d/php5-fpm.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      require => File['php5-fpm-reopenlogs'],
+    }
+  }
+  file { 'php_error_log':
+    ensure  => file,
+    path    => $php_error_log_path,
+    owner   => $php_error_log_owner,
+    group   => $php_error_log_group,
+    mode    => $php_error_log_mode,
+    require => Package['php5-fpm'],
+    notify  => Service['php5-fpm'],
   }
 }
 
